a758698fe42fa27241904304e76138645e22e9b3
Kavindu Sanjula <ravanadevs@gmail.com>
2024-03-07 00:03:03 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+
+app.use(express.json())
+
+const data = {
+  satCount: 0,
+};
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res, next) => {
+  res.render("pages/index", data);
+});
+
+app.post("/update", (req, res, next) => {
+  console.log(req.body);
+  data.satCount++;
+  res.send("OK");
+});
+
+app.listen(5000, () => {
+  console.log("server is listing on port 5000");
+});
